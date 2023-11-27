00157f52f539face4f6250b3999876dd64ea5d31
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2023-11-27 16:42:17 +0000

    Add PiranParticle class.
    This is subclassed from plasmapy.particles.Particle and does nothing
    other than add a 'density' class member.
    
    'density' refers to the number density of the respective particle within
    a plasma field (i.e. this is specifically for usage with the Cpdr
    class).
    
    Adding this allows us to query Particles.all[0].density in the same way we
    might other regular Plasmapy Particle values (e.g. Particles.all[0].charge)

--- piran/particles.py
+++ piran/particles.py
@@ -2,6 +2,12 @@ import plasmapy
 from typing import Sequence
 
 
+class PiranParticle(plasmapy.particles.Particle):
+    def __init__(self, particle: str, density: float) -> None:
+        plasmapy.particles.Particle.__init__(self, particle)
+        self.density = density
+
+
 class Particles:
     def __init__(
         self,
@@ -13,8 +19,9 @@ class Particles:
         if len(particles) != len(densities):
             raise Exception("Arguments lists should be of equal length.")
 
-        self.all = plasmapy.particles.ParticleList(particles)
-        self.densities = densities
+        self.all = [
+            PiranParticle(elem[0], elem[1]) for elem in zip(particles, densities)
+        ]
 
         self.energies = energies
         self.pitch_angles = pitch_angles
