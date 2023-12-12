2265d895f29c57da9ff0624ee55d0134c832498f
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2023-12-12 16:41:01 +0000

    Add WIP Stix class

--- piran/cpdr.py
+++ piran/cpdr.py
@@ -403,3 +403,41 @@ class Cpdr:
         # almost already in polynomial form, allowing this Sympy func to perform well.
 
         return (A * ck**4 - B * ck**2 + C).as_poly(omega)
+
+
+class Stix:
+    def __init__(
+        self, omega_p: Sequence[float], omega_c: Sequence[float]
+    ) -> None:  # numpydoc ignore=GL08
+        self._w_p = omega_p
+        self._w_c = omega_c
+
+    def R(self, w) -> float:
+        R = 1
+
+        for idx in range(len(self._w_p)):
+            R -= (self._w_p[idx] ** 2) / (w * (w + self._w_c))
+
+        return R
+
+    def L(self, w) -> float:
+        L = 1
+
+        for idx in range(len(self._w_p)):
+            L -= (self._w_p[idx] ** 2) / (w * (w - self._w_c))
+
+        return L
+
+    def P(self, w) -> float:
+        P = 1
+
+        for idx in range(len(self._w_p)):
+            P -= (self._w_p[idx] / w) ** 2
+
+        return P
+
+    def S(self, w) -> float:
+        return (self.R(w) + self.L(w)) / 2
+
+    def D(self, w) -> float:
+        return (self.R(w) - self.L(w)) / 2
