99199abc05246100b7a1897d6218755db3dbd7ed
Thomas Kappas (Advanced Research Computing) <t.kappas@bham.ac.uk>
2023-12-13 15:17:33 +0000

    Use new classes for Glauert's normalisation factor

--- piran/normalisation.py
+++ piran/normalisation.py
@@ -2,31 +2,47 @@ import numpy as np
 import sympy as sym
 from scipy.integrate import trapezoid, simpson
 
-from piran import cpdr
 from piran.resonance import replace_cpdr_symbols, poly_solver, get_valid_roots
 
 
-def compute_root_pairs(
+def solve_dispersion_relation(
+    dispersion,
+    omega_c,
+    omega_p,
     omega,
     X_range,
-    Omega_e,
-    Omega_p,
-    omega_pe,
-    omega_pp,
 ):
-    dispersion = cpdr.Cpdr(2)
+    """
+    Given wave frequency omega, solve the dispersion relation for each
+    wave normal angle X=tan(psi) in X_range to get wave number k.
 
-    CPDR_k, _ = dispersion.as_poly_in_k()
+    Parameters
+    ----------
+    dispersion : piran.cpdr.Cpdr
+        Cold plasma dispersion relation.
+    omega_c : tuple of astropy.units.quantity.Quantity
+        Cyclotron (gyro) frequencies.
+    omega_p : tuple of astropy.units.quantity.Quantity
+        Plasma frequencies.
+    omega : astropy.units.quantity.Quantity
+        Wave frequency.
+    X_range : array_like of astropy.units.quantity.Quantity
+        Wave normal angles.
 
+    Returns
+    -------
+    root_pairs : list of tuples (X, omega, k)
+        Solutions to the cold plasma dispersion relation.
+    """
     pairs = []
     for i, X in enumerate(X_range):
         values_dict = {
             "X": X.value,
             "omega": omega.value,
-            "Omega": (Omega_e.value, Omega_p.value),
-            "omega_p": (omega_pe.value, omega_pp.value),
+            "Omega": (omega_c[0].value, omega_c[1].value),
+            "omega_p": (omega_p[0].value, omega_p[1].value),
         }
-        CPDR_k2 = replace_cpdr_symbols(CPDR_k, values_dict)
+        CPDR_k2 = replace_cpdr_symbols(dispersion._poly_k, values_dict)
         k_l = poly_solver(CPDR_k2)
         valid_k_l = get_valid_roots(k_l)
 
@@ -34,7 +50,7 @@ def compute_root_pairs(
             continue
 
         if valid_k_l.size > 1:
-            msg = "We got more than one real positive root for k"
+            msg = "We got more than one real positive root for k."
             raise ValueError(msg)
 
         pairs.append((X.value, omega.value, valid_k_l[0]))
@@ -54,10 +70,23 @@ def compute_wave_norm_angle_distribution(x, mean, sd):
 def compute_glauert_normalisation_factor(
     dispersion,
     root_pairs,
-    method,
+    method="simpson",
 ):
     """
-    Placeholder
+    Calculate the normalisation factor from
+    Glauert & Horne 2005 (equation 15).
+
+    Parameters
+    ----------
+    dispersion : piran.cpdr.Cpdr
+        Cold plasma dispersion relation.
+    root_pairs : list of tuples (X, omega, k)
+        Solutions to the cold plasma dispersion relation.
+    method : str, default="simpson"
+
+    Returns
+    -------
+    norm_factor : np.floating
     """
     # Derivative in omega
     dispersion_deriv_omega = dispersion.diff("omega")
@@ -65,11 +94,13 @@ def compute_glauert_normalisation_factor(
     # Derivative in k
     dispersion_deriv_k = dispersion.diff("k")
 
+    # It has a better performance to substitute omega here since it is the
+    # same for all root pairs/triplets and then lambdify the expression outside the loop,
+    # and use the lambdified object within the loop to replace X and k.
     omega = root_pairs[0][1]  # All root pairs have the same omega
     values_dict = {
         "omega": omega,
     }
-
     dispersion_deriv_omega_eval = sym.lambdify(
         ["X", "k"], replace_cpdr_symbols(dispersion_deriv_omega, values_dict), "numpy"
     )
@@ -86,17 +117,16 @@ def compute_glauert_normalisation_factor(
         # g(X)
         dist_wave_norm = compute_wave_norm_angle_distribution(X, 0.0, 0.577)
 
-        dispersion_deriv_omega_eval2 = dispersion_deriv_omega_eval(X, k)
-        dispersion_deriv_k_eval2 = dispersion_deriv_k_eval(X, k)
-
         evaluated_integrand[i] = (
-            dist_wave_norm * k**2 * dispersion_deriv_omega_eval2 * X
-        ) / ((1 + X**2) ** (3 / 2) * dispersion_deriv_k_eval2)
+            dist_wave_norm * k**2 * dispersion_deriv_omega_eval(X, k) * X
+        ) / ((1 + X**2) ** (3 / 2) * dispersion_deriv_k_eval(X, k))
 
     if method == "trapezoid":
         integral = trapezoid(evaluated_integrand, x=X_range)
     elif method == "simpson":
         integral = simpson(evaluated_integrand, x=X_range)
+    else:
+        raise ValueError(f"Wrong integration rule: {method}")
 
     norm_factor = integral * (1 / (2 * np.pi**2))
 
--- piran/tests/test_normalisation.py
+++ piran/tests/test_normalisation.py
@@ -4,16 +4,25 @@ from astropy import constants as const
 from astropy import units as u
 from astropy.coordinates import Angle
 
-from piran import cpdr
+from piran.cpdr import Cpdr
+from piran.magfield import MagField
+from piran.particles import Particles
+from piran.particles import PiranParticle
+from piran.gauss import Gaussian
 from piran.resonance import replace_cpdr_symbols
-from piran.normalisation import compute_root_pairs, compute_glauert_normalisation_factor
+from piran.normalisation import (
+    solve_dispersion_relation,
+    compute_glauert_normalisation_factor,
+)
 
 
 class TestNormalisationFactors:
     def setup_method(self):
-        q_e = -const.e.si  # Signed electron charge
-        q_p = const.e.si  # Signed proton charge
+        frequency_ratio = 1.5 * u.dimensionless_unscaled
+        omega_ratio = 0.1225
 
+        # ============================== START ============================== #
+        # Those should be attributes of one of the main classes
         # Magnetic field
         M = 8.033454e15 * (u.tesla * u.m**3)
         mlat = Angle(0, u.deg)
@@ -22,7 +31,8 @@ class TestNormalisationFactors:
             l_shell**3 * const.R_earth**3 * math.cos(mlat.rad) ** 6
         )
 
-        frequency_ratio = 1.5 * u.dimensionless_unscaled
+        q_e = -const.e.si  # Signed electron charge
+        q_p = const.e.si  # Signed proton charge
 
         self.Omega_e = (q_e * B) / const.m_e
         Omega_e_abs = abs(self.Omega_e)
@@ -31,9 +41,47 @@ class TestNormalisationFactors:
         n_ = self.omega_pe**2 * const.eps0 * const.m_e / abs(q_e) ** 2
         self.Omega_p = (q_p * B) / const.m_p
         self.omega_pp = np.sqrt((n_ * q_p**2) / (const.eps0 * const.m_p))
+        # =============================== END =============================== #
 
-        omega_ratio = 0.1225
+        # ============================== START ============================== #
+        # We need those because they are input arguments to the new Cpdr class.
+        # They are not needed for these tests.
+        RKE = 1.0 * u.MeV  # Relativistic kinetic energy (Mega-electronvolts)
+        alpha = Angle(5, u.deg)  # pitch angle
+
+        # Lower and upper cut-off frequencies
+        omega_m = 0.35 * Omega_e_abs
+        delta_omega = 0.15 * Omega_e_abs
+        omega_lc = omega_m - 1.5 * delta_omega
+        omega_uc = omega_m + 1.5 * delta_omega
+
+        # Resonances
+        n_min = -5
+        n_max = 5
+        n_range = u.Quantity(
+            range(n_min, n_max + 1), unit=u.dimensionless_unscaled, dtype=np.int32
+        )
+        # =============================== END =============================== #
+
         self.omega = abs(self.Omega_e) * omega_ratio
+
+        piran_particle_list = (PiranParticle("e", n_), PiranParticle("H+", n_))
+        cpdr_particles = Particles(piran_particle_list, RKE, alpha)
+        # NOTE upper is just a very large number for now (X_max?)
+        cpdr_wave_angles = Gaussian(0, 1e10, 0, 0.577)
+        cpdr_wave_freqs = Gaussian(omega_lc, omega_uc, omega_m, delta_omega)
+        cpdr_mag_field = MagField(mlat, l_shell)
+        cpdr_resonances = n_range
+
+        self.dispersion = Cpdr(
+            cpdr_particles,
+            cpdr_wave_angles,
+            cpdr_wave_freqs,
+            cpdr_mag_field,
+            cpdr_resonances,
+        )
+
+        self.dispersion.as_poly_in_k()
 
     def test_glauert_normalisation_1(self):
         X_min = 0.00
@@ -43,27 +91,23 @@ class TestNormalisationFactors:
             np.linspace(X_min, X_max, X_npoints), unit=u.dimensionless_unscaled
         )
 
-        glauert_root_pairs = compute_root_pairs(
+        glauert_root_pairs = solve_dispersion_relation(
+            self.dispersion,
+            (self.Omega_e, self.Omega_p),
+            (self.omega_pe, self.omega_pp),
             self.omega,
             X_range_glauert_integral,
-            self.Omega_e,
-            self.Omega_p,
-            self.omega_pe,
-            self.omega_pp,
         )
 
-        dispersion = cpdr.Cpdr(2)
-        dispersion.as_poly_in_k()
         values_dict = {
             "Omega": (self.Omega_e.value, self.Omega_p.value),
             "omega_p": (self.omega_pe.value, self.omega_pp.value),
         }
-        dispersion_poly_k = replace_cpdr_symbols(dispersion._poly_k, values_dict)
+        dispersion_poly_k = replace_cpdr_symbols(self.dispersion._poly_k, values_dict)
 
         glauert_norm_factor = compute_glauert_normalisation_factor(
             dispersion_poly_k,
             glauert_root_pairs,
-            "simpson",
         )
 
         expected = -8.53830973e-19
@@ -77,22 +121,19 @@ class TestNormalisationFactors:
             np.linspace(X_min, X_max, X_npoints), unit=u.dimensionless_unscaled
         )
 
-        glauert_root_pairs = compute_root_pairs(
+        glauert_root_pairs = solve_dispersion_relation(
+            self.dispersion,
+            (self.Omega_e, self.Omega_p),
+            (self.omega_pe, self.omega_pp),
             self.omega,
             X_range_glauert_integral,
-            self.Omega_e,
-            self.Omega_p,
-            self.omega_pe,
-            self.omega_pp,
         )
 
-        dispersion = cpdr.Cpdr(2)
-        dispersion.as_poly_in_k()
         values_dict = {
             "Omega": (self.Omega_e.value, self.Omega_p.value),
             "omega_p": (self.omega_pe.value, self.omega_pp.value),
         }
-        dispersion_poly_k = replace_cpdr_symbols(dispersion._poly_k, values_dict)
+        dispersion_poly_k = replace_cpdr_symbols(self.dispersion._poly_k, values_dict)
 
         glauert_norm_factor = compute_glauert_normalisation_factor(
             dispersion_poly_k,
@@ -111,27 +152,23 @@ class TestNormalisationFactors:
             np.linspace(X_min, X_max, X_npoints), unit=u.dimensionless_unscaled
         )
 
-        glauert_root_pairs = compute_root_pairs(
+        glauert_root_pairs = solve_dispersion_relation(
+            self.dispersion,
+            (self.Omega_e, self.Omega_p),
+            (self.omega_pe, self.omega_pp),
             self.omega,
             X_range_glauert_integral,
-            self.Omega_e,
-            self.Omega_p,
-            self.omega_pe,
-            self.omega_pp,
         )
 
-        dispersion = cpdr.Cpdr(2)
-        dispersion.as_poly_in_k()
         values_dict = {
             "Omega": (self.Omega_e.value, self.Omega_p.value),
             "omega_p": (self.omega_pe.value, self.omega_pp.value),
         }
-        dispersion_poly_k = replace_cpdr_symbols(dispersion._poly_k, values_dict)
+        dispersion_poly_k = replace_cpdr_symbols(self.dispersion._poly_k, values_dict)
 
         glauert_norm_factor = compute_glauert_normalisation_factor(
             dispersion_poly_k,
             glauert_root_pairs,
-            "simpson",
         )
 
         expected = -8.87151913e-19
