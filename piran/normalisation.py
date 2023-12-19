010328067768635c457bb10dce1270cb50e66284
Thomas Kappas (Advanced Research Computing) <t.kappas@bham.ac.uk>
2023-12-19 15:05:14 +0000

    Replace omega and lambdify
    And this performs even better. From 10 seconds
    when we only replaced omega outside the loop,
    down to 3.5 seconds.

--- piran/normalisation.py
+++ piran/normalisation.py
@@ -160,33 +160,25 @@ def compute_cunningham_normalisation_factor(
     # Derivative in k
     dispersion_deriv_k = dispersion_poly_k.diff("k")
 
-    omega = root_pairs[0][1]  # All root pairs have the same omega
-    values_dict = {
-        "omega": omega,
-    }
-    dispersion_deriv_omega_eval = replace_cpdr_symbols(
-        dispersion_deriv_omega, values_dict
+    # Since all root pairs have the same omega,
+    # replace it outside the loop and lamdify.
+    omega = root_pairs[0][1]
+    dd_domega = sym.lambdify(
+        ["X", "k"],
+        replace_cpdr_symbols(dispersion_deriv_omega, {"omega": omega}),
+        "numpy",
     )
-    dispersion_deriv_k_eval = replace_cpdr_symbols(dispersion_deriv_k, values_dict)
+    dd_dk = sym.lambdify(
+        ["X", "k"], replace_cpdr_symbols(dispersion_deriv_k, {"omega": omega}), "numpy"
+    )
 
     norm_factor = np.empty(len(root_pairs), dtype=np.float64)
     for ii, pair in enumerate(root_pairs):
         X = pair[0]
         k = pair[2]
 
-        values_dict = {
-            "k": k,
-            "X": X,
-        }
-        dispersion_deriv_omega_eval2 = replace_cpdr_symbols(
-            dispersion_deriv_omega_eval, values_dict
-        )
-        dispersion_deriv_k_eval2 = replace_cpdr_symbols(
-            dispersion_deriv_k_eval, values_dict
-        )
-
-        norm_factor[ii] = (k**2 * dispersion_deriv_omega_eval2 * X) / (
-            (1 + X**2) ** (3 / 2) * dispersion_deriv_k_eval2
+        norm_factor[ii] = (k**2 * dd_domega(X, k) * X) / (
+            (1 + X**2) ** (3 / 2) * dd_dk(X, k)
         )
     norm_factor /= 2 * np.pi**2
 
