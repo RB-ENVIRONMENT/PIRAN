7e8549ee571924225e050965df16c31d89794c4b
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2024-01-23 10:55:11 +0000

    (WIP) add remaining numeric Stix / CPDR methods

--- piran/stix.py
+++ piran/stix.py
@@ -2,6 +2,7 @@
 Defines the Stix class.
 """
 
+from astropy import constants as const
 from astropy import units as u
 
 
@@ -102,3 +103,52 @@ class Stix:
     @u.quantity_input
     def dD(self, w: u.Hz) -> u.dimensionless_unscaled:
         return (self.dR(w) - self.dL(w)) / 2
+
+    @u.quantity_input
+    def A(self, w: u.Hz, X: u.rad) -> u.dimensionless_unscaled:
+        return (self.S(w) * X.rad**2) + self.P(w)
+
+    @u.quantity_input
+    def B(self, w: u.Hz, X: u.rad) -> u.dimensionless_unscaled:
+        return (self.R(w) * self.L(w) * X.rad**2) + (
+            (self.P(w) * self.S(w)) * (2 + X**2)
+        )
+
+    @u.quantity_input
+    def C(self, w: u.Hz, X: u.rad) -> u.dimensionless_unscaled:
+        return (self.P(w) * self.R(w) * self.L(w)) * (1 + X.rad**2)
+
+    @u.quantity_input
+    def dA(self, w: u.Hz, X: u.rad) -> u.dimensionless_unscaled:
+        return (self.dS(w) * X.rad**2) + self.dP(w)
+
+    @u.quantity_input
+    def dB(self, w: u.Hz, X: u.rad) -> u.dimensionless_unscaled:
+        return ((self.dR(w) * self.L(w) + self.R(w) * self.dL(w)) * (X.rad**2)) + (
+            (self.dP(w) * self.S(w) + self.P(w) * self.dS(w)) * (2 + X**2)
+        )
+
+    @u.quantity_input
+    def dC(self, w: u.Hz, X: u.rad) -> u.dimensionless_unscaled:
+        return (
+            self.dP(w) * self.R(w) * self.L(w)
+            + self.P(w) * self.dR(w) * self.L(w)
+            + self.P(w) * self.R(w) * self.dL(w)
+        ) * (1 + X.rad**2)
+
+    @u.quantity_input
+    def jacobian(
+        self, w: u.Hz, X: u.rad, k: u.Quantity[1 / u.m], w_c: u.Hz
+    ) -> u.dimensionless_unscaled:
+        mu = const.c * k / w_c
+        return (
+            (k**2)
+            * (
+                (
+                    (self.dA(w, X) * mu**4 - self.dB(w, X) * mu**2 + self.dC(w, X))
+                    / (2 * (2 * self.A(w, X) * mu**4 - self.B(w, X) * mu**2))
+                )
+                - (1 / w)
+            )
+            / (1 + X.rad**2)
+        )
