0ec04c6adc62c711479a88e77034d2606523cb96
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2024-02-20 10:16:47 +0000

    Switch from Hz to rad/s

--- piran/plasmapoint.py
+++ piran/plasmapoint.py
@@ -74,12 +74,14 @@ class PlasmaPoint:
     def plasma_freq(self):
         return self.__plasma_freq
 
-    def __compute_gyro_freq(self) -> Quantity[u.Hz]:
+    @u.quantity_input
+    def __compute_gyro_freq(self) -> Quantity[u.rad / u.s]:
         B = self.magpoint.flux_density
-        gf = [2 * np.pi * wc_(B, p, signed=True, to_hz=True) for p in self.particles]
+        gf = [wc_(B, p, signed=True) for p in self.particles]
         return Quantity(gf)
 
-    def __compute_plasma_freq(self) -> Quantity[u.Hz]:
+    @u.quantity_input
+    def __compute_plasma_freq(self) -> Quantity[u.rad / u.s]:
         if (
             len(self.particles) == 2
             and self.particles[0].symbol == "e-"
@@ -91,14 +93,17 @@ class PlasmaPoint:
             # This is restrictive because, for now, we only support net-zero
             # charge plasmas of electrons and protons.
             electron = self.particles[0]
-            electron_pf = abs(self.gyro_freq[0]) * self.plasma_over_gyro_ratio
+            electron_pf = np.abs(self.gyro_freq[0]) * self.plasma_over_gyro_ratio
             num_density = (
-                electron_pf**2 * const.eps0 * electron.mass / abs(electron.charge) ** 2
-            )
+                electron_pf**2
+                * const.eps0
+                * electron.mass
+                / np.abs(electron.charge) ** 2
+            ).to(1 / u.m**3, equivalencies=u.dimensionless_angles())
 
             pf = [electron_pf]
             for particle in self.particles[1:]:
-                pf.append(2 * np.pi * wp_(num_density, particle, to_hz=True))
+                pf.append(wp_(num_density, particle))
             return Quantity(pf)
         else:
             raise IllegalArgumentError("Not valid combination of input arguments")
