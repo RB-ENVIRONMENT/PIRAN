e9912f72af8c42041b34056841fef1c3a6460287
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2024-04-04 16:57:30 +0100

    Extend script to partition domain.
    Having calculated locations in X at which the number of roots in omega
    changes, we now:
    
    - Produce a `domains` list of (subdomain_lower, subdomain_upper) pairs.
    - Solve CPDR at midpoint of each subdomain.
    - Produce a num_roots list to detail number of roots within each
      subdomain.
    
    Our first example here produces a [1, 0, 1] domain - cool! This has
    highlighted two things:
    
    - Even in the '0 roots' case, our `solve_resonant` function will still
      add a 'root' to the list but populate `omega` and `k` values with
      `np.nan`. We need to catch this.
    - There's a 'middle' chunk of this domain that we can skip entirely in
      our algorithm (since there are 0 roots).

--- src/scripts/categorise_domain_in_x.py
+++ src/scripts/categorise_domain_in_x.py
@@ -42,6 +42,50 @@ def main():
 
     print(f"{X_l=}\n" f"{X_u=}\n" f"{X_all=}\n")
 
+    # Split X_all into subdomains, stored within a domains list.
+    # We could simplify this if X_all already included X_min and X_max, which would
+    # likely require them to be added during solve_resonant_for_x.
+    domains = []
+    if X_all.size == 0:
+        # No roots, so our whole domain is just [X_min, X_max]
+        domains.append(u.Quantity([X_min, X_max], u.dimensionless_unscaled))
+    else:
+        # First subdomain is X_min to our smallest root...
+        domains.append(u.Quantity([X_min, X_all[0]], u.dimensionless_unscaled))
+
+        # Grab all other subdomains in this loop
+        # nd.iter returns ndarray elements and strips units :(
+        it = np.nditer(X_all)
+        with it:
+            while not it.finished:
+                lower = it.value
+                upper = it.value if (it.iternext()) else X_max
+                domains.append(u.Quantity([lower, upper], u.dimensionless_unscaled))
+
+    print(f"Subdomains: {domains}\n")
+
+    # Now check how many roots exist in each subdomain
+    midpoints = u.Quantity(
+        [(subdomain[0] + subdomain[1]) / 2 for subdomain in domains],
+        u.dimensionless_unscaled,
+    )
+    domain_roots = cpdr.solve_resonant(midpoints)
+    print(f"Subdomain roots (at midpoints): {domain_roots}\n")
+
+    num_roots = []
+    for subdomain in domain_roots:
+
+        # Special case: if we have 1 'root', check for NaN!
+        if len(subdomain) == 1 and subdomain[0].count(np.nan):
+            num_roots.append(0)
+            continue
+
+        # Failing either of the above checks, our number of roots is just equal
+        # to the number of (X, omega, k) tuples in our subdomain list
+        num_roots.append(len(subdomain))
+
+    print(f"Roots per subdomain: {num_roots}\n")
+
 
 if __name__ == "__main__":
     main()
