4a09bb105c4aa34bc60baf7b9554328b9964a6e3
Thomas Kappas (Advanced Research Computing) <t.kappas@bham.ac.uk>
2024-06-12 17:38:13 +0100

    Compute diffusion coefficients
    Equations 8, 9 and 10 from Glauert & Horne 2005

--- src/scripts/diffusion_coefficients.py
+++ src/scripts/diffusion_coefficients.py
@@ -2,8 +2,27 @@ import argparse
 import json
 import pathlib
 
+import numpy as np
 from astropy import units as u
 from astropy.coordinates import Angle
+from scipy.integrate import simpson
+
+from piran.cpdr import Cpdr
+from piran.cpdrsymbolic import CpdrSymbolic
+from piran.diffusion import (
+    get_DnX_single_root,
+    get_normalised_intensity,
+    get_phi_squared,
+    get_power_spectral_density,
+    get_singular_term,
+)
+from piran.gauss import Gaussian
+from piran.magpoint import MagPoint
+from piran.normalisation import (
+    compute_cunningham_norm_factor,
+    compute_glauert_norm_factor,
+)
+from piran.plasmapoint import PlasmaPoint
 
 
 def main():
@@ -43,6 +62,100 @@ def main():
     method = int(parameters["method"])
 
     # ===============================================================
+
+    X_range = u.Quantity(
+        np.linspace(X_min, X_max, X_npoints), unit=u.dimensionless_unscaled
+    )
+
+    if method == 0:
+        epsilon = 0.9999  # Glauert & Horne 2005 paragraph 23
+    elif method == 1:
+        epsilon = 1.0
+    else:
+        raise Exception("Wrong method")
+
+    mag_point = MagPoint(mlat_deg, l_shell)
+    plasma_point = PlasmaPoint(mag_point, particles, plasma_over_gyro_ratio)
+    cpdr_sym = CpdrSymbolic(len(particles))
+    wave_norm_angle_dist = Gaussian(X_min, X_max, X_m, X_w)
+
+    # Calculate integral of g(X) to normalise it.
+    # Cunningham used left endpoint integration rule in his paper.
+    if method == 1:
+        integral_gx = simpson(wave_norm_angle_dist.eval(X_range), x=X_range)
+
+    for resonance in resonances:
+        DnXaa_this_res = []
+        DnXap_this_res = []
+        DnXpp_this_res = []
+
+        cpdr = Cpdr(cpdr_sym, plasma_point, energy, alpha, resonance, freq_cutoff_params)
+
+
+        resonant_roots = cpdr.solve_resonant(X_range)
+        for roots_this_x in resonant_roots:
+
+            DnXaa_this_X = 0.0
+            DnXap_this_X = 0.0
+            DnXpp_this_X = 0.0
+            for root in roots_this_x:
+
+                if np.isnan(root.omega) or np.isnan(root.k):
+                    continue
+
+                # See par.23 in Glauert & Horne 2005
+                resonance_cone_angle = -cpdr.stix.P(root.omega) / cpdr.stix.S(root.omega)
+                X_upper = min(X_max, epsilon * np.sqrt(resonance_cone_angle))
+                if root.X.value > X_upper.value:
+                    continue
+
+                if method == 0:
+                    eval_gx = wave_norm_angle_dist.eval(root.X)
+
+                    # For Glauert's norm factor we limit the range in X
+                    # between X_min and X_upper=min(X_max, epsilon*sqrt(-P/S)).
+                    X_range_glauert = X_range[X_range <= X_upper]
+
+                    norm_factor = compute_glauert_norm_factor(
+                        cpdr,
+                        root.omega,
+                        X_range_glauert,
+                        wave_norm_angle_dist,
+                        method="simpson"
+                    )
+                elif method == 1:
+                    if root.X.value == 0.0:
+                        # Avoid division by zero
+                        continue
+                    eval_gx = wave_norm_angle_dist.eval(root.X) / integral_gx
+                    norm_factor = compute_cunningham_norm_factor(
+                        cpdr,
+                        root.omega,
+                        [root.X] << root.X.unit,
+                    )[0]
+
+                power_spectral_density = get_power_spectral_density(cpdr, wave_amplitude, root.omega)
+                normalised_intensity = get_normalised_intensity(
+                    power_spectral_density, eval_gx, norm_factor
+                )
+                phi_squared = get_phi_squared(cpdr, root)
+                singular_term = get_singular_term(cpdr, root)
+
+                DnXaa_this_root, DnXap_this_root, DnXpp_this_root = get_DnX_single_root(
+                    cpdr,
+                    root,
+                    normalised_intensity,
+                    phi_squared,
+                    singular_term,
+                )
+
+                DnXaa_this_X += DnXaa_this_root.value
+                DnXap_this_X += DnXap_this_root.value
+                DnXpp_this_X += DnXpp_this_root.value
+
+            DnXaa_this_res.append(DnXaa_this_X)
+            DnXap_this_res.append(DnXap_this_X)
+            DnXpp_this_res.append(DnXpp_this_X)
 
 if __name__ == "__main__":
     main()
