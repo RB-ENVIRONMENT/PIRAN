388575a0e573efeb0afb40bf37728c071467f294
Thomas Kappas (Advanced Research Computing) <t.kappas@bham.ac.uk>
2024-06-12 17:32:10 +0100

    Take input params and assign units if needed

--- src/scripts/diffusion_coefficients.py
+++ src/scripts/diffusion_coefficients.py
@@ -2,6 +2,9 @@ import argparse
 import json
 import pathlib
 
+from astropy import units as u
+from astropy.coordinates import Angle
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -21,6 +24,25 @@ def main():
     with open(args.input) as infile:
         parameters = json.load(infile)
 
+    # ================ Parameters ===================================
+
+    particles = tuple(parameters["particles"])
+    energy = float(parameters["energy"]) << u.MeV
+    alpha = Angle(parameters["pitch_angle"], u.deg)
+    plasma_over_gyro_ratio = float(parameters["plasma_over_gyro_ratio"])
+    mlat_deg = Angle(parameters["mlat"], u.deg)
+    l_shell = float(parameters["l_shell"])
+    resonances = list(parameters["resonances"])
+    X_min = float(parameters["X_min"]) << u.dimensionless_unscaled
+    X_max = float(parameters["X_max"]) << u.dimensionless_unscaled
+    X_npoints = int(parameters["X_npoints"])
+    X_m = float(parameters["X_m"]) << u.dimensionless_unscaled
+    X_w = float(parameters["X_w"]) << u.dimensionless_unscaled
+    freq_cutoff_params = tuple(parameters["freq_cutoff_params"])
+    wave_amplitude = float(parameters["wave_amplitude"]) << u.T
+    method = int(parameters["method"])
+
+    # ===============================================================
 
 if __name__ == "__main__":
     main()
