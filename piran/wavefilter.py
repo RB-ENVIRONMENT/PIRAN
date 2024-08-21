1f5e4885b4dab0736e0358492a5cae6d160f533e
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2024-08-21 15:27:52 +0100

    add units to WaveFilter
    This will cause chaos and break things for a while.
    
    We do *need* units within this class to be able to carry out proper
    comparisons (e.g. checking that we're within upper and lower omega
    cutoffs, for example).
    
    Subsequent commits will tidy up function calls and amend tests as
    necessary to get everything back up and running.

--- piran/wavefilter.py
+++ piran/wavefilter.py
@@ -1,23 +1,41 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
+from astropy import units as u
+
+from piran.plasmapoint import PlasmaPoint
+from piran.stix import Stix
 
 
 class WaveFilter(ABC):
 
     @abstractmethod
-    def filter(self, k: np.ndarray):
+    @u.quantity_input
+    def filter(
+        self,
+        X: u.Quantity[u.dimensionless_unscaled],
+        omega: u.Quantity[u.rad / u.s],
+        k: u.Quantity[u.rad / u.m],
+        plasma: PlasmaPoint,
+        stix: Stix,
+    ) -> u.Quantity[u.rad / u.m]:
         raise NotImplementedError
 
 
 class DefaultFilter(WaveFilter):
 
+    @u.quantity_input
     def filter(
         self,
-        k: np.ndarray,
-    ):
+        X: u.Quantity[u.dimensionless_unscaled],
+        omega: u.Quantity[u.rad / u.s],
+        k: u.Quantity[u.rad / u.m],
+        plasma: PlasmaPoint,
+        stix: Stix,
+    ) -> u.Quantity[u.rad / u.m]:
+
         if k.size == 0:
-            return np.nan
+            return np.nan << u.rad / u.m
         elif k.size == 1:
             return k[0]
         else:
