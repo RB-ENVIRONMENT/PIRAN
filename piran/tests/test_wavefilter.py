e828afd8a30346aa8ed1d2fac5a9452c356c16dc
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2024-08-21 17:11:52 +0100

    add WhistlerFilter
    Including tests using overdense and underdense plasmas.
    
    This is currently throwing the following warnings:
    
    DeprecationWarning: Conversion of an array with ndim > 0 to a scalar is deprecated,
    and will error in future. Ensure you extract a single element from your array
    before performing this operation. (Deprecated NumPy 1.25.)

--- piran/tests/test_wavefilter.py
+++ piran/tests/test_wavefilter.py
@@ -1,11 +1,55 @@
+import math
+
+import numpy as np
 import pytest
 from astropy import units as u
 from astropy.coordinates import Angle
 
+from piran.cpdr import Cpdr
+from piran.cpdrsymbolic import CpdrSymbolic
+from piran.helpers import get_real_and_positive_roots
 from piran.magpoint import MagPoint
 from piran.plasmapoint import PlasmaPoint
 from piran.stix import Stix
-from piran.wavefilter import WaveFilter
+from piran.wavefilter import WaveFilter, WhistlerFilter
+
+
+@u.quantity_input
+def calculate_omega_L0(
+    omega_p: u.Quantity[u.rad / u.s], omega_c: u.Quantity[u.rad / u.s]
+) -> u.Quantity[u.rad / u.s]:
+    # Z-mode waves first appear when L=0.
+    # Rearranging L=0 yields a quadratic in omega with coefficients
+    # (-omega_p**2, -omega_c, 1)
+    #
+    # (Note that Gurnett&Bhattacharjee notation uses unsigned omega_c)
+    #
+    # We call the positive solution to this quadratic omega_L0.
+    #
+    # Near omega_L0...
+    # For omega < omega_L0 we expect to see one (Whistler) solution.
+    # For omega > omega_L0 we expect to see two solutions (Whistler and Z-mode).
+    #
+    # The ordering of omega_L0, omega_p, omega_c changes based on the plasma density.
+    #
+    # Two things are always true:
+    # - omega_c is FIXED (and assumed positive/unsigned in this case).
+    # - omega_p is always larger than omega_L0.
+    #
+    # But...
+    # For omega_p > sqrt(2)*omega_c we have omega_p > omega_L0 > omega_c
+    # For sqrt(2)*omega_c > omega_p > omega_c we have omega_p > omega_c > omega_L0
+    # For omega_c > omega_p we have omega_c > omega_p > omega_L0
+    #
+    # i.e in overdense cases (omega_p > omega_c), omega_L0 is larger than omega_c
+    # unless omega_p < sqrt(2)*omega_c.
+    #
+    # In underdense cases, omega_L0 is always smaller than omega_c.
+    #
+    # How do additional ions influence this?...
+    L_quadratic = np.polynomial.Polynomial([-omega_p.value**2, -omega_c.value, 1])
+    L0 = L_quadratic.roots()
+    return L0[L0 > 0] << u.rad / u.s
 
 
 class TestWaveFilter:
@@ -71,3 +115,99 @@ class TestWaveFilter:
 
         with pytest.raises(NotImplementedError):
             InheritedFilter().filter(X, omega, k, plasma, stix)
+
+    def test_whistler_1(self):
+
+        # Overdense plasma
+        plasma_over_gyro_ratio = 1.5
+        plasma = PlasmaPoint(self.mag_point, self.particles, plasma_over_gyro_ratio)
+        stix = Stix(plasma.plasma_freq, plasma.gyro_freq)
+
+        X = 0.3165829145728643 << u.dimensionless_unscaled
+        omega = 21197.313961282573 << u.rad / u.s
+        k = 0.00024206540583296198 << u.rad / u.m
+
+        whistlers = WhistlerFilter()
+
+        # Omega below omega_L0 (when Z-mode first appears)
+        omega_p = plasma.plasma_freq[0]
+        omega_c = plasma.gyro_freq[0]
+        assert omega < calculate_omega_L0(omega_p, omega_c)
+
+        k_after_filtering = whistlers.filter(X, omega, k, plasma, stix)
+
+        # We have one whistler mode wave that is unchanged by our filter
+        assert k == k_after_filtering
+
+    def test_whistler_2(self):
+
+        # Underdense plasma
+        plasma_over_gyro_ratio = 0.75
+        plasma = PlasmaPoint(self.mag_point, self.particles, plasma_over_gyro_ratio)
+        stix = Stix(plasma.plasma_freq, plasma.gyro_freq)
+
+        n_particles = len(self.particles)
+        cpdr_sym = CpdrSymbolic(n_particles)
+
+        energy = 1.0507018 * u.MeV
+        alpha = Angle(0.125, u.deg)
+        resonance = -5
+        freq_cutoff_params = (0.35, 0.15, -1.5, 1.5)
+        cpdr = Cpdr(
+            cpdr_sym,
+            plasma,
+            energy,
+            alpha,
+            resonance,
+            freq_cutoff_params,
+            WhistlerFilter(),
+        )
+
+        # We need an omega value larger than omega_L0 to be confident we are looking at both
+        # whistler and z-mode waves
+        omega_L0 = calculate_omega_L0(plasma.plasma_freq[0], plasma.gyro_freq[0])
+
+        X = 0.259 << u.dimensionless_unscaled
+
+        ### Method 1. ###
+        ### Hardcoded omega (correspond to X above), bypassing cpdr.solve_resonant
+        omega = 34357.65267505 << u.rad / u.s
+
+        # Confirm we are above omega_L0 but below omega_p
+        assert plasma.plasma_freq[0] > omega > omega_L0
+
+        cpdr_in_k_omega = cpdr.poly_in_k.subs(
+            {
+                "X": X.value,
+                "omega": omega.value,
+            }
+        )
+
+        # Solve unmodified CPDR to obtain k roots for given X, omega
+        k_l = np.roots(cpdr_in_k_omega.as_poly().all_coeffs())
+
+        # Keep only real and positive roots
+        valid_k_l = get_real_and_positive_roots(k_l) << u.rad / u.m
+
+        # Confirm we have 2 roots
+        assert valid_k_l.size == 2
+
+        k_after_filtering = WhistlerFilter().filter(X, omega, valid_k_l, plasma, stix)
+
+        # Confirm that we have 1 solution after filtering...
+        assert k_after_filtering.size == 1
+
+        # ... and that it is the largest solution (since index of refraction is greater for whistlers than z-mode)
+        assert all(k_after_filtering >= valid_k_l)
+
+        ### Method 2. ###
+        ### Via cpdr.solve_resonant
+
+        resonant_roots = cpdr.solve_resonant(X)
+
+        # We only expect one soln for this X
+        # (using prior knowledge)
+        assert len(resonant_roots) == 1
+
+        # Check that it is the same soln as found via method 1
+        assert math.isclose(resonant_roots[0][0].k.value, k_after_filtering.value)
--- piran/wavefilter.py
+++ piran/wavefilter.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
+from astropy import constants as const
 from astropy import units as u
 
 from piran.plasmapoint import PlasmaPoint
@@ -41,3 +42,32 @@ class DefaultFilter(WaveFilter):
         else:
             msg = "We got more than one real positive root for k"
             raise ValueError(msg)
+
+
+class WhistlerFilter(WaveFilter):
+
+    def filter(
+        self,
+        X: u.Quantity[u.dimensionless_unscaled],
+        omega: u.Quantity[u.rad / u.s],
+        k: u.Quantity[u.rad / u.m],
+        plasma: PlasmaPoint,
+        stix: Stix,
+    ) -> u.Quantity[u.rad / u.m]:
+
+        # Frequency for Whistlers does not exceed electron gyro frequency
+        if omega > abs(plasma.gyro_freq[0]):
+            return np.nan << u.rad / u.m
+
+        # The square of the index of refraction is:
+        # - bounded by R below
+        # - unbounded above
+
+        k = np.atleast_1d(k)
+
+        # Calculate index of refraction for all k.
+        # Exclude any k for which index of refraction does not exceed R.
+        mu2 = (const.c * k / omega) ** 2
+        k = k[mu2 > stix.R(omega)]
+
+        return k if k.size > 0 else np.nan << u.rad / u.m
