Fix scripts after adding units everywhere

src/scripts/cunningham_figure5b.py
@@ -32,7 +32,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy import units as u
 
-from piran.diffusion import get_diffusion_coefficients, get_energy_diffusion_coefficient
+from piran.diffusion import (
+    UNIT_DIFF,
+    get_diffusion_coefficients,
+    get_energy_diffusion_coefficient,
+)
 
 
 def calc_DEE_over_E_squared(pathname):
@@ -47,17 +51,16 @@ def calc_DEE_over_E_squared(pathname):
         with open(file, "r") as f:
             results = json.load(f)
 
-        X_range = np.array(results["X_range"])
-        alpha = results["pitch_angle"]
-        resonances = results["resonances"]
+        X_range = np.array(results["X_range"]) << u.dimensionless_unscaled
+        alpha = results["pitch_angle"] << u.deg
         DnXpp = results["DnXpp"]
         rest_mass_energy_J = results["rest_mass_energy_Joule"] << u.J
         rel_kin_energy_J = (results["rel_kin_energy_MeV"] << u.MeV).to(u.J)
 
         Dpp = 0.0
-        for i, resonance in enumerate(resonances):
-            DnXpp_this_res = np.array(DnXpp[i])
-            integral = get_diffusion_coefficients(X_range, DnXpp_this_res)
+        # Loop over resonances (n)
+        for DnXpp_this_res in DnXpp:
+            integral = get_diffusion_coefficients(X_range, DnXpp_this_res << UNIT_DIFF)
             Dpp += integral
 
         Dee = get_energy_diffusion_coefficient(
@@ -69,8 +72,8 @@ def calc_DEE_over_E_squared(pathname):
 
     # Sort by pitch angle
     sorted_vals = sorted(zip(pitch_angle, Dee_over_e_squared), key=lambda z: z[0])
-    xx = [z[0] for z in sorted_vals]
-    yy = [z[1] for z in sorted_vals]
+    xx = [z[0].value for z in sorted_vals]
+    yy = [z[1].value for z in sorted_vals]
 
     return (xx, yy)
 

src/scripts/cunningham_figure6.py
@@ -36,9 +36,10 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+from astropy import units as u
 from matplotlib.ticker import FuncFormatter, LogFormatterMathtext
 
-from piran.diffusion import get_diffusion_coefficients
+from piran.diffusion import UNIT_DIFF, get_diffusion_coefficients
 
 
 def calc_Daa_over_p_squared(pathname):
@@ -53,23 +54,23 @@ def calc_Daa_over_p_squared(pathname):
         with open(file, "r") as f:
             results = json.load(f)
 
-        X_range = np.array(results["X_range"])
+        X_range = np.array(results["X_range"]) << u.dimensionless_unscaled
         DnXaa = results["DnXaa"]
         momentum = results["momentum"]
 
         Daa = 0.0
         # Loop over resonances (n)
         for DnXaa_this_res in DnXaa:
-            integral = get_diffusion_coefficients(X_range, np.array(DnXaa_this_res))
+            integral = get_diffusion_coefficients(X_range, DnXaa_this_res << UNIT_DIFF)
             Daa += integral
 
-        energy.append(results["rel_kin_energy_MeV"])
+        energy.append(results["rel_kin_energy_MeV"] << u.MeV)
         Daa_over_p_squared.append(Daa / momentum**2)
 
     # Sort by energy
     sorted_vals = sorted(zip(energy, Daa_over_p_squared), key=lambda z: z[0])
-    xx = [z[0] for z in sorted_vals]
-    yy = [z[1] for z in sorted_vals]
+    xx = [z[0].value for z in sorted_vals]
+    yy = [z[1].value for z in sorted_vals]
 
     return (np.array(xx), np.array(yy))
 