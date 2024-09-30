d320895c5fa4946858d388e4b821ca9f969dc380
James Tyrrell (Advanced Research Computing) <j.m.tyrrell@bham.ac.uk>
2024-09-30 16:13:05 +0100

    fix test_validators.py
    I've had to swap from using MagPoint (which no longer uses check_units
    decorator) to a small dedicated class that does use check_units.
    
    The principle remains the same.

--- piran/tests/test_validators.py
+++ piran/tests/test_validators.py
@@ -1,46 +1,55 @@
 import pytest
 from astropy import units as u
 from astropy.coordinates import Angle
+from astropy.units import Quantity
 
-from piran.magpoint import MagPoint
-from piran.validators import set_global_type_checking
+from piran.validators import check_units, set_global_type_checking
+
+
+class CheckUnits:
+
+    @check_units
+    def __init__(
+        self,
+        quant: Quantity[u.rad],
+    ) -> None:
+        self.__quant = quant.to(u.rad)
 
 
 class TestValidators:
 
     def test_check_units_1(self):
-        l_shell = 4.5
-        mlat_quantity = Angle(45 * u.deg)
-        mlat_int = 45
+        a_quantity = Angle(45 * u.deg)
+        an_int = 45
 
         # Type-checking for MagPoint should be on/True by default.
         # This should behave:
-        MagPoint(mlat_quantity, l_shell)
+        CheckUnits(a_quantity)
 
         # This should fail type-checking and raise the following exception:
-        # TypeError: Argument 'mlat' to function '__init__' has no 'unit' attribute.
+        # TypeError: Argument 'quant' to function '__init__' has no 'unit' attribute.
         # You should pass in an astropy Quantity instead.
         with pytest.raises(TypeError):
-            MagPoint(mlat_int, l_shell)
+            CheckUnits(an_int)
 
         # Lets turn type-checking off
         set_global_type_checking(False)
 
         # This should still behave:
-        MagPoint(mlat_quantity, l_shell)
+        CheckUnits(a_quantity)
 
         # This should now bypass type-checking and raise a later exception in the body
         # of __init__:
         # AttributeError: 'int' object has no attribute 'to'
         with pytest.raises(AttributeError):
-            MagPoint(mlat_int, l_shell)
+            CheckUnits(an_int)
 
         # Double-check that toggling type-checking back on works and that we see the
-        # same results as before. In particular, we want to check that using `mlat_int`
-        # returns to raising a `TypeError` (since mlat_quantity behaves in all cases).
+        # same results as before. In particular, we want to check that using `an_int`
+        # returns to raising a `TypeError` (since a_quantity behaves in all cases).
         set_global_type_checking(True)
 
-        MagPoint(mlat_quantity, l_shell)
+        CheckUnits(a_quantity)
 
         with pytest.raises(TypeError):
-            MagPoint(mlat_int, l_shell)
+            CheckUnits(an_int)
