0204283981fa5bb3ef6086f559acdba70c5d9333
Thomas Kappas (Advanced Research Computing) <t.kappas@bham.ac.uk>
2025-01-10 11:53:52 +0000

    Improved docstrings for the following modules:
    gauss
    helpers
    magpoint
    plasmapoint

--- piran/magpoint.py
+++ piran/magpoint.py
@@ -1,5 +1,10 @@
 """
-Defines the MagPoint class.
+The `magpoint` module provides a class for representing the magnetic field
+of a planet (defaulting to Earth) at a specific point in space. It calculates
+the magnetic flux density based on magnetic latitude, L-shell value, planetary
+radius, and magnetic dipole moment.
+
+This module uses the dipole approximation for the planetary magnetic field.
 """
 
 import numpy as np
@@ -10,10 +15,11 @@ from astropy.units import Quantity
 
 class MagPoint:
     """
-    A representation of the magnetic field of a planet (by default Earth's),
-    at a specific point defined by magnetic latitude and L-shell value.
-    Given magnetic latitude, L-shell value, radius and magnetic dipole moment,
-    we compute the magnetic flux density and store it in self.flux_density.
+    Represents the magnetic field of a planet (by default Earth's) at a
+    specific point defined by magnetic latitude and L-shell value.
+
+    This class uses the dipole approximation to calculate the magnetic flux
+    density.
 
     Parameters
     ----------
@@ -21,14 +27,34 @@ class MagPoint:
         The magnetic latitude, given in units convertible to radians.
 
     l_shell : float
-        The "L-shell", "L-value", or "McIlwain L-parameter".
+        The "L-shell", "L-value", or "McIlwain L-parameter". A dimensionless
+        quantity representing the radial distance of a magnetic field line
+        from the center of the planet, normalised to the planetary radius.
 
     planetary_radius : astropy.units.Quantity[u.m], default=astropy.constants.R_earth
         The radius of the planet of interest, given in units convertible to meters.
+        Defaults to Earth's radius.
 
     mag_dipole_moment : astropy.units.Quantity[u.tesla * u.m**3], default=8.033454e15 * (u.tesla * u.m**3)
         The magnetic dipole moment of the planet of interest, given in units convertible
-        to tesla metres cubed.
+        to Tesla metres cubed (T m^3). Defaults to Earth's magnetic dipole moment (8.033454e15 T m^3).
+
+    Attributes
+    ----------
+    flux_density : astropy.units.Quantity[u.tesla]
+        The magnetic flux density vector at the specified point, as an
+        `astropy.units.Quantity` with units of Tesla.
+
+    Notes
+    -----
+    The magnetic field is calculated using the dipole model, as described in
+    Glauert & Horne (2005), paragraph 13.
+
+    Examples
+    --------
+    >>> mlat_deg = Angle(0 * u.deg)
+    >>> l_shell = 4.5 * u.dimensionless_unscaled
+    >>> mag_point = MagPoint(mlat_deg, l_shell)
     """
 
     @u.quantity_input
--- piran/plasmapoint.py
+++ piran/plasmapoint.py
@@ -1,5 +1,9 @@
 """
-Defines the PlasmaPoint class.
+The `plasmapoint` module provides a class for representing a quasineutral plasma
+at a specific point in space. It calculates key plasma parameters like
+plasma frequency and gyrofrequency based on particle composition and
+either the plasma-to-gyro ratio for electrons or number densities for each
+species.
 """
 
 import numpy as np
@@ -12,13 +16,18 @@ from plasmapy.particles import ParticleList, ParticleListLike
 from piran.magpoint import MagPoint
 
 
-class IllegalArgumentError(ValueError):
-    pass
-
-
 class PlasmaPoint:
     """
-    A representation of a quasineutral charge plasma given a list of particles.
+    Represents a quasineutral plasma at a specific point in space.
+
+    This class calculates plasma parameters, including plasma frequency
+    and gyrofrequency, based on the particle composition and provided
+    input. It supports two main use cases:
+
+    1. Electron-proton plasmas, where the plasma-to-gyro ratio for
+       electrons is provided.
+    2. Plasmas with an arbitrary number of species, where the number
+       density for each species is provided.
 
     Parameters
     ----------
@@ -29,8 +38,44 @@ class PlasmaPoint:
         A list-like collection of plasmapy particle-like objects.
 
     plasma_over_gyro_ratio : float | None, default=None
+        The ratio of plasma frequency to gyrofrequency for electrons.
+        Supported if the plasma consists of electrons and protons only.
 
     number_density : astropy.units.Quantity[1 / u.m**3] | None, default=None
+        A list or array of number densities for each particle species (in m^-3).
+        Required if the plasma contains more than two species or if it does not
+        contain exactly electron and proton.
+
+    Attributes
+    ----------
+    gyro_freq : astropy.units.Quantity[u.rad / u.s]
+        An array containing the gyrofrequency for each species. The order of
+        the frequencies corresponds to the order of the particles in the
+        `particles` list.
+    plasma_freq : astropy.units.Quantity[u.rad / u.s]
+        An array containing the plasma frequency for each species. The order
+        of the frequencies corresponds to the order of the particles in the
+        `particles` list.
+
+    Raises
+    ------
+    ValueError
+        If the input parameters are inconsistent. For example:
+            * If neither `plasma_over_gyro_ratio` nor `number_density` is provided.
+            * If *both* `plasma_over_gyro_ratio` and `number_density` are provided.
+            * If `plasma_over_gyro_ratio` is provided but `particles` are not
+              exactly electron and proton.
+
+    Examples
+    --------
+    >>> mag_point = MagPoint(Angle(0 * u.deg), 4.5)
+    >>> particles = ("e", "H+")
+    >>> plasma_point = PlasmaPoint(mag_point, particles, plasma_over_gyro_ratio=1.5)
+
+    >>> mag_point = MagPoint(Angle(0 * u.deg), 4.5)
+    >>> particles = ("e", "p+")
+    >>> num_density = [2524781.78, 2524781.78] * (1 / u.m**3)
+    >>> plasma_point = PlasmaPoint(mag_point, particles, number_density=num_density)
     """
 
     @u.quantity_input
@@ -50,10 +95,10 @@ class PlasmaPoint:
 
         # =================================================================
         # Currently we support two use cases. We either expect an electron
-        # and proton plasma and plasma over gyro ration for electrons or
+        # and proton plasma and plasma over gyro ratio for electrons or
         # any number of species and a list of their number densities.
         # Based on these we calculate plasma frequencies and here we
-        # calculate plasma over gyro ration and number densities for all
+        # calculate plasma over gyro ratio and number densities for all
         # species in both cases.
         if self.number_density is not None and self.plasma_over_gyro_ratio is None:
             self.__plasma_over_gyro_ratio = np.abs(self.plasma_freq / self.gyro_freq)
@@ -137,4 +182,4 @@ class PlasmaPoint:
                 pf.append(wp_(self.number_density[i], particle))
             return Quantity(pf)
         else:
-            raise IllegalArgumentError("Not valid combination of input arguments")
+            raise ValueError("Not valid combination of input arguments")
