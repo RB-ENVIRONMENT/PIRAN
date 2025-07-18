ba504bd74a853cfd7914d328b2859102508d16e5
James Tyrrell <j.m.tyrrell@bham.ac.uk>
2025-07-18 16:15:32 +0100

    Quantity -> Angle for consistency
    The Angle class is derived from Quantity, so any code that works for a
    Quantity should continue to work when replaced by an Angle.
    
    Using Angle gives us access to other convenience methods (e.g. like .deg
    or .rad) and should probably be preferred. I'm not sure we've employed
    this consistently across the whole codebase.

--- src/scripts/bounce_averaged_diffusion_coefficients.py
+++ src/scripts/bounce_averaged_diffusion_coefficients.py
@@ -267,7 +267,7 @@ def main():
     # Create integration range for equations 24, 25 and 26 in Glauert 2005
     lambda_min = 0.0 << u.rad
     lambda_max = bounce.mirror_latitude << u.rad
-    lambda_range = u.Quantity(
+    lambda_range = Angle(
         np.linspace(lambda_min, lambda_max, mlat_npoints), unit=u.rad
     )
     container["mlat_range"] = lambda_range.value.tolist()
