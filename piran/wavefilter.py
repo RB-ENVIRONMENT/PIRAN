7bafa3b2570f99e966ea7cf6353c4e4da1ba8fac
James Tyrrell <j.m.tyrrell@bham.ac.uk>
2025-11-30 12:15:22 +0000

    stricter lower bound for WhistlerFilter...
    ... in particular, this was previously just the proton lower hybrid
    frequency. It's now the max of the proton lower hybrid frequency or the
    proton gyrofrequency. Adding this in response to weird results observed
    (multiple k for a given omega) during bounce averaging simulations.

--- piran/wavefilter.py
+++ piran/wavefilter.py
@@ -108,10 +108,13 @@ class WhistlerFilter(WaveFilter):
         plasma: PlasmaPoint,
         stix: Stix,
     ) -> bool:
-        # Frequency for Whistlers is bounded below by the lower hybrid frequency for protons
-        # and bounded above by the electron plasma- or gyro-frequency (whichever is smaller)
+        # Frequency for Whistlers is bounded:
+        # - below by the proton lower hybrid frequency or gyrofrequency
+        #   (whichever is larger)
+        # - above by the electron plasma- or gyro-frequency
+        #   (whichever is smaller)
         if (
-            not plasma.lower_hybrid_freq[1]
+            not max(plasma.lower_hybrid_freq[1], abs(plasma.gyro_freq[1]))
             <= omega
             <= min(abs(plasma.gyro_freq[0]), abs(plasma.plasma_freq[0]))
         ):
